81717491180dbd79e0b5429bc491685a53dcd920
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2022-09-02 13:52:13 +0700

    Support GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,24 @@ export default defineConfig({
 	root: './edge',
 	build: {
 		outDir: '../docs',
+		assetsDir: './',
 		sourcemap: process.env.NODE_ENV === 'production',
 		chunkSizeWarningLimit: Infinity,
 	},
 	resolve: {
 		alias: {
 			'@mui/icons': '@mui/icons/esm',
+		},
+	},
+	experimental: {
+		// See https://vitejs.dev/guide/build.html#advanced-base-options
+		renderBuiltUrl: (fileName, { hostType }) => {
+			if (hostType === 'html') {
+				// Avoid the leading / in the paths because it will not work when deploying to GitHub Pages
+				return fileName
+			} else {
+				return { relative: true }
+			}
 		},
 	},
 })
